748cb32e691d14ac21f3e2d65a36f9e2e8ae1609
4dagio <4dagio.01@gmail.com>
2021-10-02 09:51:23 -0500

    Update images with environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'aefc7125742296e8015a42320c1719a5',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
 
 /*
